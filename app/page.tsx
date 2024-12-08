b37aa8f1715a5fe4b0aa134482f2ccbb5d0d78a7
MoudyH <moudyh89@gmail.com>
2024-12-08 15:02:08 +0200

    tesssssssst

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
         <Hero/>
-        <h1>test</h1>
+        <h1>tessssssst</h1>
       </main>
     </div>
   );
