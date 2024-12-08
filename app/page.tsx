829225cd32473619ad03e33a544cf2d2c51adbe5
MoudyH <moudyh89@gmail.com>
2024-12-08 14:59:23 +0200

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "@/components/Hero";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
         <Hero/>
+        <h1>test</h1>
       </main>
     </div>
   );
